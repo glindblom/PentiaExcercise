fc5baee4a350ce036b644d70cf80999fe28fe105
glindblom <lindblom.gustav@gmail.com>
2016-09-16 13:32:01 +0200

    Added basic MVC skeleton

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace PentiaExcercise
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseStartup<Startup>()
+                    .Build();
+
+            host.Run();
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PentiaExcercise
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder application)
+        {
+            application.UseMvc(config => {
+                config.MapRoute
+                (
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+    }
+}
